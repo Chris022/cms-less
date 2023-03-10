ce1dc0bdc1adcdbf60f3a2ea6f010bfc45450dea
Christoph <christoph@debian>
2023-03-10 22:39:16 +0100

    add comments to autolaod

--- src/autoload.php
+++ src/autoload.php
@@ -1,7 +1,10 @@
 <?php
 namespace cms_less;
 
+//autoload all classes
 spl_autoload_register(function ($class) {
+    //remove cms_less from the file-path, since it is the root namespace and does not exist as a folder!
+    //replace \ in the file-path with the system Directory Seperator (/ on linux)
     $file = __DIR__.DIRECTORY_SEPARATOR.
         str_replace("cms_less","",
             str_replace('\\', DIRECTORY_SEPARATOR, $class)
